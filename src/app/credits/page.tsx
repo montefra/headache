cf97b276cad23f8ae8a29786d7a6db7fc81cbfc8
Francesco Montesano <franz.bergesund@gmail.com>
2025-03-08 21:04:38 +0100

    Add icon and credit page

---
+++ src/app/credits/page.tsx
@@ -0,0 +1,12 @@
+export default function Credits() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl pb-8">Credits</h1>
+      <ul className="list-disc text-lg" >
+        <li>
+          <a href="https://www.freepik.com" className="text-blue-400 visited:text-purple-400">freepik</a> for the brain logo
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Welcome</h1>
+    <div className="flex flex-col min-h-screen justify-between items-center">
+      <div className="flex flex-col items-center mt-4">
+        <Image src="/brainstorming.png" alt="Logo" width={200} height={200} />
+        <h1 className="text-4xl mt-4">Welcome</h1> {/* Added margin-top */}
+      </div>
+      <div className="mb-4">
+        <Link href={"/credits"}>Credits</Link>
+      </div>
+    </div>
   );
 }
